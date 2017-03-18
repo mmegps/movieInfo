797b99246339ee4df6b9117167cfa95917deaadf
mmegps <mmegps@gmail.com>
2017-03-18 15:09:13 +0000

    Add support for individual movie pages.

--- js/main.js
+++ js/main.js
@@ -18,7 +18,7 @@ function getMovies(searchText) {
         <div class="well text-center">
            <img src="${movie.Poster}">
            <h5>${movie.Title}</h5>
-           <a onclick="movieSelected('${movie.imdbID}')" class="btn btn-primary" href="#"></a>
+           <a onclick="movieSelected('${movie.imdbID}')" class="btn btn-primary" href="#">Movie Details</a>
         </div>
       </div>
       `;
